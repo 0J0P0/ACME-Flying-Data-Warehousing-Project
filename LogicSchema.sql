c86d746aaef304146d99018c6740dad6e273134b
JP <jpniznhy@hotmail.com>
2023-10-02 13:49:04 +0200

    AircraftUtilization

--- LogicSchema.sql
+++ LogicSchema.sql
@@ -1,12 +1,21 @@
 CREATE TABLE AircraftUtilization (
     aircraftID CHAR(6),
     timeID DATE,
-    flightHours NUMBER(2),
-    flightCycles NUMBER(2),
+    flightHours NUMBER(2),  -- FH
+    flightCycles NUMBER(2), -- TO
     PRIMARY KEY (aircraftID, timeID),
     FOREIGN KEY (aircraftID) REFERENCES AircraftDimension(ID),
     FOREIGN KEY (timeID) REFERENCES TemporalDimension(ID)
 );
+
+CREATE TABLE b (
+
+)
+
+
+-- ---------------- --
+-- DIMENSION TABLES
+-- ---------------- --
 
 CREATE TABLE AircraftDimension (
     ID CHAR(6),
@@ -17,7 +26,14 @@ CREATE TABLE AircraftDimension (
 
 CREATE TABLE TemporalDimension (
     ID DATE,
+    day NUMBER(2) NOT NULL,
     month CHAR(7) NOT NULL,
     year NUMBER(4) NOT NULL,
     PRIMARY KEY (ID),
+);
+
+CREATE TABLE AirportDimension (
+    ID CHAR(3),
+    airport VARCHAR2(100) NOT NULL,
+    PRIMARY KEY (ID)
 );
