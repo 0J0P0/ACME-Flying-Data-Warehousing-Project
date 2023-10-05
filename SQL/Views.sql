4ccc76e295e5312ded4edb4cdaf6dcce35254dad
JP <jpniznhy@hotmail.com>
2023-10-05 20:32:10 +0200

    view

--- SQL/Views.sql
+++ SQL/Views.sql
@@ -1,3 +1,11 @@
 ------------------------------------------------------------------------
 -- Views
 ------------------------------------------------------------------------
+
+CREATE VIEW a AS (
+    SELECT a.ID, t.ID, SUM(m.flight_hours_count), SUM(m.take_offs_count)
+    FROM AircraftDimension a, TemporalDimension t, miketowers m
+    WHERE a.ID = m.aircraftID AND t.ID = m.timeID
+    GROUP BY a.ID, t.ID
+    ORDER BY a.ID, t.ID
+) WITH CHECK OPTION;
