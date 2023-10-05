6e94d1e9eab0d61fd94117061c27b2b7cfd409a8
JP <jpniznhy@hotmail.com>
2023-10-05 17:59:44 +0200

    com

--- SQL/KPIs.sql
+++ SQL/KPIs.sql
@@ -1,6 +1,19 @@
+------------------------------------------------------------------------
+-- KPIs.sql
+------------------------------------------------------------------------
+
+
+-- TABLE KPIs-1
 -- FH (flight hours) = flights.actualarrival - flights.actualdeparture
-
 -- TO (take offs) = COUNT(flights.actualdeparture)
--- TO (take offs) = COUNT(flights.cancelled == FALSE)
+-- DY
+-- CN
+    -- DYR (delay rate)
+    -- CNR (cancellation rate)
+    -- TDY
+    -- ADD
 
+
+-- TABLE KPIs-2
 -- ADOS (aircraft days out of service) = SUM(ADOSS) + SUM(ADOSU)
+-- AIDS (aircraft in days service)
