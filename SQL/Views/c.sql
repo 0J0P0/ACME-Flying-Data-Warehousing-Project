96cfd2613bdfeb0eef3e997ba789de61683fed2f
Carlos Arbonés Sotomayor <carlos.arbones@estudiantat.upc.edu>
2023-10-07 11:48:18 +0200

    s

--- SQL/Views/c.sql
+++ SQL/Views/c.sql
@@ -2,14 +2,14 @@ CREATE VIEW c
 AS
   (
   SELECT a.ID, t.ID,
-    SUM()*1000 as RRh,
+    (SUM()/SUM())*1000 as RRh,
     SUM()*1000 as RRc,
     SUM()*1000 as PRRh,
     SUM()*1000 as PRRc,
     SUM()*1000 as MRRh,
     SUM()*1000 as MRRc
-  FROM AircraftDimension a, TemporalDimension t, AircraftUtilizationMetrics aum
-  WHERE a.ID = aum.aircraftID AND t.ID = aum.timeID
+  FROM AircraftDimension a, TemporalDimension t, LogBookMetrics lbm
+  WHERE a.ID = lbm.aircraftID AND t.ID = lbm.timeID
   GROUP BY a.ID, t.ID
   ORDER BY a.ID, t.ID
 ) WITH CHECK OPTION;
