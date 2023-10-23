ae397d07fa463bee32e6b4fd10705faafc730d53
JP <jpniznhy@hotmail.com>
2023-10-23 20:25:31 +0200

    carlitos

--- SQL/Queries.sql
+++ SQL/Queries.sql
@@ -1,58 +1,52 @@
-------------------------------------------------------------------------
--- Queries
-------------------------------------------------------------------------
-
--- Give me FH and TO per aircraft (also per model) per day (also per month and per year)
-SELECT a."aircraftID",
-        t."dateID",
-        ROUND(SUM(aum.flight_hours_count / 60)::NUMERIC, 4) AS flight_hours,
-        SUM(aum.take_offs_count) AS take_offs
-FROM "AircraftUtilizationMetrics" aum, "AircraftDimension" a, "TemporalDimension" t
-WHERE a."aircraftID" = aum."aircraftID" AND t."dateID" = aum."dateID"
-GROUP BY a."aircraftID", t."dateID"
-ORDER BY a."aircraftID", t."dateID";
+-- a) Give me FH and TO per aircraft (also per model) per day (also per month and per year)
+SELECT aumv."aircraftID",
+        aumv."dateID",
+        ROUND(SUM(aumv.flight_hours_count / 60)::NUMERIC, 4) AS flight_hours,
+        SUM(aumv.take_offs_count) AS take_offs
+FROM "AircraftUtilizationMetricsView" aumv
+GROUP BY aumv."aircraftID", aumv."dateID"
+ORDER BY aumv."aircraftID", aumv."dateID";
 
 
 
--- Give me ADIS, ADOS, ADOSS, ADOSU, DYR, CNR, TDR, ADD per aircraft (also per model) per month (also per year)
-SELECT a."aircraftID",
-        t."month",
-        ROUND(SUM(aum.days_in_service / 1440)::NUMERIC, 4) AS "ADIS",
-        ROUND(SUM(aum.days_out_of_service_scheduled + aum.days_out_of_service_unscheduled)::NUMERIC, 4) AS "ADOS",
-        ROUND(SUM(aum.days_out_of_service_scheduled)::NUMERIC, 4) AS "ADOSS",
-        ROUND(SUM(aum.days_out_of_service_unscheduled)::NUMERIC, 4) AS "ADOSU",
+-- b) Give me ADIS, ADOS, ADOSS, ADOSU, DYR, CNR, TDR, ADD per aircraft (also per model) per month (also per year)
+SELECT aumv."aircraftID",
+        aumv."month",
+        ROUND(SUM(aumv.days_in_service / (24*60))::NUMERIC, 4) AS "ADIS",
+        ROUND(SUM(aumv.days_out_of_service_scheduled + aumv.days_out_of_service_unscheduled)::NUMERIC, 4) AS "ADOS",
+        ROUND(SUM(aumv.days_out_of_service_scheduled)::NUMERIC, 4) AS "ADOSS",
+        ROUND(SUM(aumv.days_out_of_service_unscheduled)::NUMERIC, 4) AS "ADOSU",
         CASE
-                WHEN SUM(aum.take_offs_count) > 0
-                THEN 100 * SUM(aum.delay_count)/SUM(aum.take_offs_count)
+                WHEN SUM(aumv.take_offs_count) > 0
+                THEN 100 * SUM(aumv.delay_count)/SUM(aumv.take_offs_count)
                 ELSE 0
         END AS "DYR",
         CASE
-                WHEN SUM(aum.take_offs_count) > 0
-                THEN 100 * SUM(aum.cancelled_count)/SUM(aum.take_offs_count)
+                WHEN SUM(aumv.take_offs_count) > 0
+                THEN 100 * SUM(aumv.cancelled_count)/SUM(aumv.take_offs_count)
                 ELSE 0
         END AS "CNR",
         CASE
-                WHEN SUM(aum.take_offs_count) > 0
-                THEN 100 * (100 - (SUM(aum.delay_count) + SUM(aum.cancelled_count))/SUM(aum.take_offs_count))
+                WHEN SUM(aumv.take_offs_count) > 0
+                THEN 100 * (100 - (SUM(aumv.delay_count) + SUM(aumv.cancelled_count))/SUM(aumv.take_offs_count))
                 ELSE 0
         END AS "TDR",
         CASE
-                WHEN SUM(aum.delay_count) > 0
-                THEN ROUND((100 * SUM(aum.delay_min_count)/SUM(aum.delay_count))::NUMERIC, 4)
+                WHEN SUM(aumv.delay_count) > 0
+                THEN ROUND((100 * SUM(aumv.delay_min_count)/SUM(aumv.delay_count))::NUMERIC, 4)
                 ELSE 0
         END AS "ADD"
-FROM "AircraftUtilizationMetrics" aum, "AircraftDimension" a, "TemporalDimension" t
-WHERE a."aircraftID" = aum."aircraftID" AND t."dateID" = aum."dateID"
-GROUP BY a."aircraftID", t."month"
-ORDER BY a."aircraftID", t."month";     
+FROM "AircraftUtilizationMetricsView" aumv
+GROUP BY aumv."aircraftID", aumv."month"
+ORDER BY aumv."aircraftID", aumv."month";     
 
 
 -- c) Give me the RRh, RRc, PRRh, PRRc, MRRh and MRRc per aircraft (also per model and manufacturer) per month (also per year)
-SELECT lbmv."aircraftID",
+SELECT lbmv."manufacturer",
         lbmv.month,
         CASE
                 WHEN SUM(lbmv.flight_hours_count) > 0
-                THEN 1000 * SUM(lbmv.logbook_count)/SUM(lbmv.flight_hours_count)
+                THEN ROUND((1000 * SUM(lbmv.logbook_count)/SUM(lbmv.flight_hours_count))::NUMERIC, 4)
                 ELSE 0
         END AS "RRh",
         CASE
@@ -62,17 +56,17 @@ SELECT lbmv."aircraftID",
         END AS "RRc",
         CASE
                 WHEN SUM(lbmv.flight_hours_count) > 0
-                THEN 1000 * SUM(lbmv.logbook_count*(lbmv.reporteurclass='PIREP')::INTEGER)/SUM(lbmv.flight_hours_count)
+                THEN  ROUND(((1000 * SUM(lbmv.logbook_count*(lbmv.reporteurclass='PIREP')::INTEGER)/SUM(lbmv.flight_hours_count))::NUMERIC), 4)
                 ELSE 0
         END AS "PRRh",
         CASE
                 WHEN SUM(lbmv.take_offs_count) > 0
-                THEN 100 * SUM(lbmv.logbook_count*(lbmv.reporteurclass='PIREP')::INTEGER)/SUM(lbmv.take_offs_count)
+                THEN  100 * SUM(lbmv.logbook_count*(lbmv.reporteurclass='PIREP')::INTEGER)/SUM(lbmv.take_offs_count)
                 ELSE 0
         END AS "PRRc",
         CASE
                 WHEN SUM(lbmv.flight_hours_count) > 0
-                THEN 1000 * SUM(lbmv.logbook_count*(lbmv.reporteurclass='MAREP')::INTEGER)/SUM(lbmv.flight_hours_count)
+                THEN  ROUND(((1000 * SUM(lbmv.logbook_count*(lbmv.reporteurclass='MAREP')::INTEGER)/SUM(lbmv.flight_hours_count))::NUMERIC), 4)
                 ELSE 0
         END AS "MRRh",
         CASE
@@ -81,23 +75,24 @@ SELECT lbmv."aircraftID",
                 ELSE 0
         END AS "MRRc"
 FROM "LogBookMetricsView" lbmv
-GROUP BY lbmv."aircraftID", lbmv.month
-ORDER BY lbmv."aircraftID", lbmv.month;
+GROUP BY lbmv."manufacturer", lbmv.month
+ORDER BY lbmv."manufacturer", lbmv.month;
 
 
--- Give me the MRRh and MRRc per airport of the reporting person per aircraft (also per model)
+-- d) Give me the MRRh and MRRc per airport of the reporting person per aircraft (also per model)
 SELECT lbmv."aircraftID",
         lbmv.airport,
         CASE
-                WHEN SUM(lbmv.flight_hours_count) > 0 AND lbmv.reporteurclass = 'MAREP'
-                THEN 1000 * SUM(lbmv.logbook_count)/SUM(lbmv.flight_hours_count)
+                WHEN SUM(lbmv.flight_hours_count) > 0
+                THEN ROUND((1000 * SUM(lbmv.logbook_count)/SUM(lbmv.flight_hours_count))::NUMERIC, 4)
                 ELSE 0
         END AS "MRRh",
         CASE
-                WHEN SUM(lbmv.take_offs_count) > 0 AND lbmv.reporteurclass = 'MAREP'
+                WHEN SUM(lbmv.take_offs_count) > 0
                 THEN 100 * SUM(lbmv.logbook_count)/SUM(lbmv.take_offs_count)
                 ELSE 0
         END AS "MRRc"
 FROM "LogBookMetricsView" lbmv
+WHERE lbmv.reporteurclass = 'MAREP'
 GROUP BY lbmv."aircraftID", lbmv.airport
 ORDER BY lbmv."aircraftID", lbmv.airport;
--- SQL/Views.sql
+++ SQL/Views.sql
@@ -1,19 +1,53 @@
-------------------------------------------------------------------------
--- Views
-------------------------------------------------------------------------
-
--- DROP VIEW IF EXISTS "LogBookMetricsView";
-CREATE OR REPLACE MATERIALIZED VIEW "LogBookMetricsView" AS (
-    SELECT a."aircraftID",
+-- DROP MATERIALIZED VIEW IF EXISTS "AircraftUtilizationMetricsView";
+CREATE MATERIALIZED VIEW "AircraftUtilizationMetricsView" AS (
+    SELECT a."aircraftID", 
             a.aircraft_model,
             a.manufacturer,
+            t."dateID",
             t.month,
             t.year,
-            ad.airport,
-            ad.reporteurclass,
             aum.flight_hours_count,
             aum.take_offs_count,
+            aum.delay_count,
+            aum.delay_min_count,
+            aum.cancelled_count,
+            aum.days_in_service,
+            aum.days_out_of_service_scheduled,
+            aum.days_out_of_service_unscheduled
+    FROM "AircraftUtilizationMetrics" aum, "AircraftDimension" a, "TemporalDimension" t
+    WHERE a."aircraftID" = aum."aircraftID" AND t."dateID" = aum."dateID"
+)
+
+
+-- DROP MATERIALIZED VIEW IF EXISTS "LogBookMetricsView";
+CREATE MATERIALIZED VIEW "LogBookMetricsView" AS (
+    SELECT aumv."aircraftID",
+            aumv.aircraft_model,
+            aumv.manufacturer,
+            aumv.month,
+            aumv.year,
+            ad.airport,
+            ad.reporteurclass,
+            aumv.flight_hours_count,
+            aumv.take_offs_count,
             lbm.logbook_count
-    FROM "AircraftUtilizationMetrics" aum, "LogBookMetrics" lbm, "AircraftDimension" a, "TemporalDimension" t, "AirportDimension" ad
-    WHERE a."aircraftID" = aum."aircraftID" AND t."dateID" = aum."dateID" AND ad."reporteurID" = lbm."reporteurID"
+    FROM "AircraftUtilizationMetricsView" aumv, "LogBookMetrics" lbm, "AirportDimension" ad
+    WHERE ad."reporteurID" = lbm."reporteurID" AND aumv."dateID" = lbm."dateID" AND aumv."aircraftID" = lbm."aircraftID"
 );
+
+
+-- -- DROP VIEW IF EXISTS "LogBookMetricsView";
+-- CREATE MATERIALIZED VIEW "LogBookMetricsView" AS (
+--     SELECT a."aircraftID",
+--             a.aircraft_model,
+--             a.manufacturer,
+--             t.month,
+--             t.year,
+--             ad.airport,
+--             ad.reporteurclass,
+--             aum.flight_hours_count,
+--             aum.take_offs_count,
+--             lbm.logbook_count
+--     FROM "AircraftUtilizationMetrics" aum, "LogBookMetrics" lbm, "AircraftDimension" a, "TemporalDimension" t, "AirportDimension" ad
+--     WHERE a."aircraftID" = aum."aircraftID" AND t."dateID" = aum."dateID" AND ad."reporteurID" = lbm."reporteurID"
+-- );
